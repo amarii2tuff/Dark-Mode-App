3b847ad03a3b80e59aac601b6a4b8765ca0aa9b4
amarii2tuff <lyndon.edwards@hgs.hiddengeniusproject.org>
2025-10-16 20:38:47 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+function darkMode() {
+
+    const body = document.body;
+
+   
+    if (body.style.backgroundColor === 'black') {
+ 
+        body.style.backgroundColor = 'white';
+        body.style.color = 'black';
+    } else {
+   
+        body.style.backgroundColor = 'black';
+        body.style.color = 'white';
+    }
+
+}
